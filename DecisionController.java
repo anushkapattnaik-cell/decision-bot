8e4a8ef8313c899502289818cc348ed6e2e1f37f
Anushka Pattnaik <anushkapattnaik143@gmail.com>
2026-01-30 21:46:23 +0530

    Update DecisionController.java

--- DecisionController.java
+++ DecisionController.java
@@ -1,13 +1,17 @@
 package com.decisionbot.app;
 
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class DecisionController {
 
+    //taking the user input (Here taking the user input basically means answers of the questions like yes/no).
     Scanner in = new Scanner(System.in);
 
+    //here we are creating an array for the constructor "Question" and storing the value in the "Questions".
     Question[] Questions = {
+
+            //syntax : {new array_name(value which were initialised in the Question class in the parameterized constructor will be filled here.)
+            // new Question(String,int)
             new Question("Is this important for your life choices?", 3),
             new Question("Will this help you in future?",3),
             new Question("Is it risky?",-2),
@@ -16,7 +20,9 @@ public class DecisionController {
     };
 
     public void start(){
+        //initially the score would be 0
         int score = 0;
+        //using the for-each loop
         for(Question q : Questions){
             System.out.println(q.text + " (yes/no)");
             String ans = in.nextLine().toLowerCase();
@@ -28,6 +34,7 @@ public class DecisionController {
             }
         }
 
+        //calling the function to show the result
         showResult(score);
     }
     private void showResult(int score){
