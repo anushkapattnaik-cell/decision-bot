919bf0f6ca9c2936ec0627f47927edd72c30c128
Anushka Pattnaik <anushkapattnaik143@gmail.com>
2026-01-30 21:47:13 +0530

    Update Question.java

--- Question.java
+++ Question.java
@@ -4,6 +4,7 @@ public class Question {
     String text;
     int weight;
 
+    //constructor in created.
     Question(String text, int weight) {
         this.text = text;
         this.weight = weight;
